c4ce45ac16f596e3b3799e8199dd34d8cb00ffbb
Conatus <cona_tus@naver.com>
2023-10-16 15:07:06 +0900

    Layout Component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import SkipNav from './SkipNav';
 
 export default function Layout() {
   return (
     <>
+      <SkipNav />
       <Navbar />
       <Outlet />
     </>
