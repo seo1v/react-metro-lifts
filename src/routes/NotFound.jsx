2d20856229dea17f80303c702dbb05965e3ddaaf
Conatus <cona_tus@naver.com>
2023-10-18 10:35:07 +0900

    Create NotFound Component

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './NotFound.module.css';
 
 export default function NotFound() {
-  return <div>Notfound</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>페이지를 찾을 수 없습니다.</h2>
+    </div>
+  );
 }
