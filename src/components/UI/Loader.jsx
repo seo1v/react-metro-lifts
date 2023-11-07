53b44c5dc7b11d3855872dd66b77d2ea2a1f2db9
Conatus <cona_tus@naver.com>
2023-11-07 09:22:54 +0900

    Create Loader component

--- src/components/UI/Loader.jsx
+++ src/components/UI/Loader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './Loader.module.css';
 
 export default function Loader() {
-  return <div>Loading...</div>;
+  return <div className={styles.loader}></div>;
 }
