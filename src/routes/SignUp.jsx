ccdb295e4d21be9739e2aaaa7f555de0e3919764
Conatus <cona_tus@naver.com>
2023-11-07 09:14:13 +0900

    Change SignUp component for redesign

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { auth } from '../api/firebase';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { useForm } from 'react-hook-form';
 import { BiSolidUser, BiSolidEnvelope, BiSolidLockAlt } from 'react-icons/bi';
 import Modal from '../components/ui/Modal';
 import Logo from '../components/ui/Logo';
 import styles from './SignUp.module.css';
 
-const env = process.env;
-env.PUBLIC_URL = env.PUBLIC_URL || '';
-
 export default function SignUp() {
+  const user = auth.currentUser ?? null;
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
@@ -23,11 +21,15 @@ export default function SignUp() {
   } = useForm({ mode: 'onBlur' });
   let passwordCurrent = watch('password', '');
 
+  // 사용자가 존재하면 home으로 리다이렉트
+  if (user) {
+    return <Navigate to='/' />;
+  }
+
   const onSubmit = async (data) => {
     setError('');
 
     try {
-      // 로딩 시작
       setIsLoading(true);
 
       // 계정 생성
@@ -36,25 +38,22 @@ export default function SignUp() {
         data.email,
         data.password
       );
-      console.log(credentials.user);
 
       // 사용자 이름 프로필에 등록
       await updateProfile(credentials.user, {
         displayName: data.username,
-        photoURL: process.env.PUBLIC_URL + '/images/logo.png',
+        photoURL: process.env.REACT_APP_DEFAULT_AVATAR,
       });
 
       // Home으로 이동
       navigate('/');
     } catch (e) {
-      // 에러 처리
       setError(
         e.code === 'auth/email-already-in-use'
           ? '이미 존재하는 이메일입니다.'
           : e.code
       );
     } finally {
-      // 로딩 끝
       setIsLoading(false);
     }
   };
@@ -63,8 +62,8 @@ export default function SignUp() {
     <div className={styles.wrapper}>
       <aside className={styles.bg}></aside>
       <section className={styles.section}>
-        <header>
-          <Logo />
+        <header className={styles.header}>
+          <Logo size='big' />
           <div className={styles.text}>
             <h1 className={styles.title}>만나서 반가워요!</h1>
             <p className={styles.subtitle}>
@@ -73,111 +72,114 @@ export default function SignUp() {
             </p>
           </div>
         </header>
-        <legend className={styles.legend}>회원가입</legend>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <div className={styles.contents}>
-            <label htmlFor='username'>사용자 이름</label>
-            <div className={styles.content}>
-              <input
-                type='text'
-                placeholder='이름을 입력하세요.'
-                {...register('username', {
-                  required: '이름을 입력하세요.',
-                  minLength: {
-                    value: 3,
-                    message: '이름은 3자 이상 입력해야 합니다.',
-                  },
-                  maxLength: {
-                    value: 10,
-                    message: '이름은 10자 이내로 입력해야 합니다.',
-                  },
-                  pattern: {
-                    value:
-                      /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){1,8}[a-zA-Z0-9]$/,
-                    message:
-                      '이름에 사용가능한 문자: 영문 대소문자, 숫자, 점(.), 밑줄(_), 하이픈(-)',
-                  },
-                })}
-              />
-              <BiSolidUser />
+        <main className={styles.main}>
+          <legend className={styles.legend}>회원가입</legend>
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.contents}>
+              <label htmlFor='username'>사용자 이름</label>
+              <div className={styles.content}>
+                <input
+                  type='text'
+                  placeholder='이름을 입력하세요.'
+                  {...register('username', {
+                    required: '이름을 입력하세요.',
+                    minLength: {
+                      value: 3,
+                      message: '이름은 3자 이상 입력해야 합니다.',
+                    },
+                    maxLength: {
+                      value: 10,
+                      message: '이름은 10자 이내로 입력해야 합니다.',
+                    },
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){1,8}[a-zA-Z0-9]$/,
+                      message:
+                        '이름에 사용가능한 문자: 영문 대소문자, 숫자, 점(.), 밑줄(_), 하이픈(-)',
+                    },
+                  })}
+                />
+                <BiSolidUser />
+              </div>
+              {errors.username && (
+                <p className={styles.error}>{errors.username.message}</p>
+              )}
             </div>
-            {errors.username && (
-              <p className={styles.error}>{errors.username.message}</p>
-            )}
-          </div>
-          <div className={styles.contents}>
-            <label htmlFor='email'>이메일</label>
-            <div className={styles.content}>
-              <input
-                type='email'
-                placeholder='wherever@example.com'
-                {...register('email', {
-                  required: '이메일을 입력하세요.',
-                  pattern: {
-                    value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
-                    message: '이메일을 올바른 형식으로 입력하세요.',
-                  },
-                })}
-              />
-              <BiSolidEnvelope />
+            <div className={styles.contents}>
+              <label htmlFor='email'>이메일</label>
+              <div className={styles.content}>
+                <input
+                  type='email'
+                  placeholder='wherever@example.com'
+                  {...register('email', {
+                    required: '이메일을 입력하세요.',
+                    pattern: {
+                      value:
+                        /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                      message: '이메일을 올바른 형식으로 입력하세요.',
+                    },
+                  })}
+                />
+                <BiSolidEnvelope />
+              </div>
+              {errors.email && (
+                <p className={styles.error}>{errors.email.message}</p>
+              )}
             </div>
-            {errors.email && (
-              <p className={styles.error}>{errors.email.message}</p>
-            )}
-          </div>
-          <div className={styles.contents}>
-            <label htmlFor='password'>비밀번호</label>
-            <div className={styles.content}>
-              <input
-                type='password'
-                placeholder='비밀번호를 입력하세요.'
-                {...register('password', {
-                  required: '비밀번호를 입력하세요.',
-                  minLength: {
-                    value: 8,
-                    message:
-                      '비밀번호는 영어 대소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.',
-                  },
-                  pattern: {
-                    value:
-                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-                    message:
-                      '비밀번호는 영어 대소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.',
-                  },
-                })}
-              />
-              <BiSolidLockAlt />
+            <div className={styles.contents}>
+              <label htmlFor='password'>비밀번호</label>
+              <div className={styles.content}>
+                <input
+                  type='password'
+                  placeholder='비밀번호를 입력하세요.'
+                  {...register('password', {
+                    required: '비밀번호를 입력하세요.',
+                    minLength: {
+                      value: 8,
+                      message:
+                        '비밀번호는 영어 대소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.',
+                    },
+                    pattern: {
+                      value:
+                        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                      message:
+                        '비밀번호는 영어 대소문자, 숫자, 특수문자를 포함해 8자 이상이어야 합니다.',
+                    },
+                  })}
+                />
+                <BiSolidLockAlt />
+              </div>
+              {errors.password && (
+                <p className={styles.error}>{errors.password.message}</p>
+              )}
             </div>
-            {errors.password && (
-              <p className={styles.error}>{errors.password.message}</p>
-            )}
-          </div>
-          <div className={styles.contents}>
-            <label htmlFor='passwordConfirm'>비밀번호 확인</label>
-            <div className={styles.content}>
-              <input
-                type='password'
-                placeholder='비밀번호를 다시 입력하세요.'
-                {...register('passwordConfirm', {
-                  required: '비밀번호를 다시 입력하세요.',
-                  validate: (value) =>
-                    value === passwordCurrent ||
-                    '비밀번호가 일치하지 않습니다.',
-                })}
-              />
-              <BiSolidLockAlt />
+            <div className={styles.contents}>
+              <label htmlFor='passwordConfirm'>비밀번호 확인</label>
+              <div className={styles.content}>
+                <input
+                  type='password'
+                  placeholder='비밀번호를 다시 입력하세요.'
+                  {...register('passwordConfirm', {
+                    required: '비밀번호를 다시 입력하세요.',
+                    validate: (value) =>
+                      value === passwordCurrent ||
+                      '비밀번호가 일치하지 않습니다.',
+                  })}
+                />
+                <BiSolidLockAlt />
+              </div>
+              {errors.passwordConfirm && (
+                <p className={styles.error}>{errors.passwordConfirm.message}</p>
+              )}
             </div>
-            {errors.passwordConfirm && (
-              <p className={styles.error}>{errors.passwordConfirm.message}</p>
-            )}
-          </div>
-          <input type='submit' value='Sign Up' />
-          {error !== '' ? <p className={styles.error}>{error}</p> : null}
-        </form>
+            <input type='submit' value='회원가입' />
+            {error !== '' ? <p className={styles.error}>{error}</p> : null}
+          </form>
+        </main>
         <footer className={styles.footer}>
           <p className={styles.switcher}>
             이미 계정을 보유하고 계신가요? &#x2004;
-            <Link to='/signin'>Sign in &rarr;</Link>
+            <Link to='/signin'>로그인 &rarr;</Link>
           </p>
         </footer>
       </section>
