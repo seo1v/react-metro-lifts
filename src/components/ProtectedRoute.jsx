2695ec525bed0553f5b0dab6fd28b5c0452f4821
Conatus <cona_tus@naver.com>
2023-11-07 09:15:20 +0900

    Create Protected Route component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { auth } from '../api/firebase';
+import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
+  const user = auth.currentUser;
+
+  // 사용자가 존재하지 않으면 로그인 페이지로 리다이렉션
+  if (user === null) {
+    return <Navigate to='/signin' />;
+  }
+
   return children;
 }
