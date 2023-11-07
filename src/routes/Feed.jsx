7afd664cda30b5ef70a752a95c5b939a9967902c
Conatus <cona_tus@naver.com>
2023-11-07 09:16:59 +0900

    Create Feed component

--- src/routes/Feed.jsx
+++ src/routes/Feed.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import PostForm from '../components/PostForm';
+import TimeLine from '../components/TimeLine';
+import Wrapper from '../components/Wrapper';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Aside from '../components/ui/Aside';
+import { useScrollToTop } from '../hooks/useScrollToTop';
+import ScrollButton from '../components/ui/ScrollButton';
 
 export default function Feed() {
-  return <section>Feed</section>;
+  const { showButton, onScrollToTop } = useScrollToTop();
+
+  return (
+    <Wrapper>
+      <Aside />
+      <Header>
+        <PostForm />
+      </Header>
+      <Main>
+        <TimeLine />
+      </Main>
+      {showButton && <ScrollButton onScroll={onScrollToTop} />}
+    </Wrapper>
+  );
 }
