288e0bbc1780eaf27f2b18879026722a5a5fdc57
seo1v <165138611+seo1v@users.noreply.github.com>
2024-05-27 17:18:35 +0900

    Update Feed.jsx

--- src/routes/Feed.jsx
+++ src/routes/Feed.jsx
@@ -4,9 +4,9 @@ import TimeLine from '../components/TimeLine';
 import Wrapper from '../components/Wrapper';
 import Header from '../components/Header';
 import Main from '../components/Main';
-import Aside from '../components/ui/Aside';
+import Aside from '../components/ui/Aside.jsx';
 import { useScrollToTop } from '../hooks/useScrollToTop';
-import ScrollButton from '../components/ui/ScrollButton';
+import ScrollButton from '../components/ui/ScrollButton.jsx';
 
 export default function Feed() {
   const { showButton, onScrollToTop } = useScrollToTop();
